11b62da934461a3bc8f829e62e384c01e52b1755
Noah Case <casenoah908@gmail.com>
2021-11-22 17:15:42 -0600

    A Small Start
    Three modules aligned in different spaces. the user review box submits user text into a value in the ts file, the results tab shows a table with some custom data, and the individual word box is hardly started.

--- HCI-Project/src/app/app.component.ts
+++ HCI-Project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Word } from './Word';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HCI-Project';
+
+  userReview = '';
+
+  //This is your value for submitted Reviews
+  submittedReview: string = '';
+
+  //a custom assignment to the list the table will show. I will split this up later this week.
+  words: Word[] = [
+    {name: 'apple', favorable: 65.74, unfavorable: 34.26},
+    {name: 'noah', favorable: 65.74, unfavorable: 34.26},
+    {name: 'test', favorable: 65.74, unfavorable: 34.26}
+  ]
+
+  submitReview(review: string){
+    this.submittedReview = review;
+  }
 }
