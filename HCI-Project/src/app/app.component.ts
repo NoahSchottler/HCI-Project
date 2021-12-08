fed1f433039a8ec42cd7ffc17a4b810dcd9419cc
Noah Schottler <noahschottler1@gmail.com>
2021-12-07 22:37:40 -0600

    trains classifier on page load, this prevents lag from occurring during the first classification

--- HCI-Project/src/app/app.component.ts
+++ HCI-Project/src/app/app.component.ts
@@ -9,6 +9,8 @@ import {LoaddataService} from './loaddata.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  //LOCAL PROPERTIES
   nb = new NaiveBayesClassifier();
   TrainingData: Object[] = [];
   title = 'HCI-Project';
@@ -48,10 +50,14 @@ export class AppComponent {
   // Math.round( (100 + result.probability + Number.EPSILON)*100)/100;
 
   /**
-   * Instantiates service
+   * Instantiates service, trains data on startup
    * @param service
    */
-  constructor(private service: LoaddataService) {}
+  constructor(private service: LoaddataService) {
+    this.train().then(res => {
+      console.log('Initial Training Completed');
+    });
+  }
 
   /**
    * submits user review and splits each word into the userWords array
@@ -63,7 +69,6 @@ export class AppComponent {
     if(!this.trained){
       this.train().then(r => {
         this.classify(this.submittedReview);
-        this.trained = true;
       });
     }
     else{
@@ -76,9 +81,13 @@ export class AppComponent {
    * Trains data from text file.
    */
   async train() {
+    // Grab training data
     let data = this.GetTrainingData();
+
+    // Once promise resolves, run the data through the classifier
     await data.then(res => {
       this.nb.train(res);
+      this.trained = true;
       console.log('Training Completed');
     });
   }
@@ -90,9 +99,15 @@ export class AppComponent {
   classify(input: string) {
     console.log('Starting Classification');
     this.IsWait = false;
+
+    // Categorizes the input through the classifier
     let classification = this.nb.categorize(input);
+
+    // Sets local probability to results
     this.probability = classification.probability;
     this.roundedConfidence = (Math.round((100+this.probability)*100)/100);
+
+    // Sets local category string to classification result
     if(classification.category == "1"){
       console.log('Classification: Positive (Fresh) Review');
       this.category = 'Positive';
@@ -102,6 +117,8 @@ export class AppComponent {
       this.category = 'Negative';
     }
     console.log('Confidence: ' + (100 + this.probability) + "%");
+
+    // Classifies each word from the input string for display in the per-word breakdown
     this.userWords = this.submittedReview.split(' ');
     this.words = [];
     this.userWords.forEach(value => {
@@ -146,30 +163,46 @@ export class AppComponent {
         }
       });
     });
+
+    // We set the TrainingData in order to be used globally
     this.TrainingData = TrainingDataLocal;
+
+
     return TrainingDataLocal;
   }
 
+  /**
+   * Search for a single word and updates the single word search properties to display
+   * @param input word to classify
+   */
   searchWord(input: string){
     this.searchedWord = input;
     this.searchedFlag = true;
+
+    // Catorgorize input through Naive Bayes
     let result = this.nb.categorize(input)
-    // this.searchedConfidence = 100 + result.probability;
     this.searchedConfidence = Math.round( (100 + result.probability + Number.EPSILON)*100)/100;
+
+    // Set local properties to result
     if(result.category == "1"){
       this.searchedCategory = 'Positive';
     }
     if(result.category == "0"){
       this.searchedCategory = 'Negative';
     }
-    let localTrainingData: { category: string; text: string };
+
+    // Begin iteration through training data to find occurrences
     let count = 0;
     let negCount = 0;
     let posCount = 0;
     this.TrainingData.forEach(value => {
       let arrayText: any;
+
+      // Split into individual words
       // @ts-ignore
       arrayText = value.text.split(' ');
+
+      // Observe categories, increment if input word matches an occurrence in the data
       // @ts-ignore
       if(value.category == "1"){
         arrayText.forEach((value: string) => {
@@ -190,6 +223,7 @@ export class AppComponent {
       }
 
     });
+    // Update local properties
     this.appearedCount = count;
     this.appearedFavorableCount = posCount;
     this.appearedUnfavorableCount = negCount;
